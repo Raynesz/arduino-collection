c5de1d7b1ebd8b2369d501eb8e806df81bdcb55a
Raynesz <31424806+Raynesz@users.noreply.github.com>
2025-01-29 02:43:33 +0200

    theremin and key press frequencies are combined

--- music_instrument/music_instrument.ino
+++ music_instrument/music_instrument.ino
@@ -1,26 +1,52 @@
 int notes[] = {262, 294, 330, 349};
 
+int sensorValue;
+int sensorLow = 1023;
+int sensorHigh = 0;
+
+const int ledPin = 13; // on-board LED
+
 void setup() {
   Serial.begin(9600);
+
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, HIGH); // turn the LED on to signal calibration start
+
+  while (millis() < 5000) { // calibrates the phototransistor in the first 5 seconds of operation
+    sensorValue = analogRead(A2);
+    if (sensorValue > sensorHigh) {
+      sensorHigh = sensorValue;
+    }
+    if (sensorValue < sensorLow) {
+      sensorLow = sensorValue;
+    }
+  }
+
+  digitalWrite(ledPin, LOW); // turn the LED off to signal calibration end
 }
 
 void loop() {
-  int keyVal = analogRead(A0);
-  Serial.println(keyVal);
+  int keyVal = analogRead(A0); // get key press value
+  sensorValue = analogRead(A2); // get phototransistor value
 
+  int thereminPitch = map(sensorValue, sensorLow, sensorHigh, 50, 500); // bind theremin pitch within certain range
+
+  int finalFrequency = thereminPitch;
+
+  // combine theremin and key press frequencies
   if (keyVal == 1023) {
-    tone(8, notes[0]);
+    finalFrequency = (thereminPitch + notes[0]) / 2;
   }
   else if (keyVal >= 990 && keyVal <= 1010) {
-    tone(8, notes[1]);
+    finalFrequency = (thereminPitch + notes[1]) / 2;
   }
   else if (keyVal >= 505 && keyVal <= 515) {
-    tone(8, notes[2]);
+    finalFrequency = (thereminPitch + notes[2]) / 2;
   }
   else if (keyVal >= 5 && keyVal <= 10) {
-    tone(8, notes[3]);
+    finalFrequency = (thereminPitch + notes[3]) / 2;
   }
-  else {
-    noTone(8);
-  }
-}
+
+  tone(8, finalFrequency); // play the frequency. piezo is on pin 8
+  delay(10);
+}                                                           
